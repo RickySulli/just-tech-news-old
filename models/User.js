c30d615e0a4e3aba111d874ec6c2d1cffe65caef
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-01-07 02:18:54 -0600

    comparing hashed passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, Datatypes, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create our USER model
 class User extends Model {}
@@ -45,6 +46,19 @@ User.init(
       }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            // set up beforeUPdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+          },
+
       sequelize,
       timestamps: false,
       freezeTableName: true,
@@ -52,5 +66,7 @@ User.init(
       modelName: 'user'
     }
   );
+
+
 module.exports = User;
 
