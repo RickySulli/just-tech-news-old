f23e01bc61fc60de8c323dedadcb94387970fe76
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-01-09 03:00:24 -0600

    worked til it didn't on 4-5

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('../user-routes.js');
 const postRoutes = require('../api/post-routes');
 
 router.use('/users', userRoutes);
