e7b3fe0985d50cfe484ad2d4de9271ba91d32f40
RickySulli <71475646+RickySulli@users.noreply.github.com>
2021-01-23 01:57:21 -0600

    14.1 snapshot to start fresh

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+const hbs = exphbs.create({});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-//turn on routes
-app.use(routes);
 
-//turn on connection to db and server
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-sequelize.sync({ force: false}).then(() => {
-    app.listen(PORT, () => console.log('Now Listening'));
+app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
